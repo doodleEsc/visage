a79d153cea4418e47fee30e0f4e7a4d59104b722
fanlizhou <lizhou.fan@midea.com>
2017-04-21 16:34:34 +0800

    add a Dispatcher Module to call method

--- visage/dispatch/dispatcher.py
+++ visage/dispatch/dispatcher.py
@@ -2,8 +2,45 @@
 this module dispatch the request to backend
 """
 
+from visage.utils.importutils import import_object
 
 class Dispatcher(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, *args):
+        """
+        :param args: 
+        """
+        backends = {}
+
+        for item in args:
+            print item
+            backends[item] = import_object(item)
+
+        self.backends = backends
+
+    def __call__(self, request):
+        backend_name = request.get('backend')
+        method_name = request.get('execute')
+        arguments = request.get('arguments')
+
+        backend = self.backends[backend_name]
+        method = getattr(backend, method_name)
+
+        resp = method(**arguments)
+
+        return resp
+
+if __name__ == '__main__':
+
+    backends = [
+        'visage.backend.test.Test'
+    ]
+
+    request = {
+        'backend': 'visage.backend.test.Test',
+        'execute': 'test',
+        'arguments': {}
+    }
+
+    d = Dispatcher(*backends)
+    print d(request)
