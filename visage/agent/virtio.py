7c550a2e0515610e1918907ae756b0839f49977e
fanlz <cokie@foxmail.com>
2017-05-18 00:36:46 +0800

    add timeout while read and write both in block and nonblock

--- visage/agent/virtio.py
+++ visage/agent/virtio.py
@@ -1,34 +1,37 @@
-#!/usr/bin/python
 # -*- coding:utf-8 -*-
-import fcntl, signal, os, time, select, errno
+import fcntl, signal, os, time, select, errno, sys
 from pysigset import SIGSET, sigemptyset, sigaddset,\
     sigprocmask, SIG_BLOCK, SIG_UNBLOCK
 from visage.utils import log
+from visage.common.timeout import timeout, TimeoutError
 
 LOG = log.Log()
 
 
 class ChannelAgent(object):
 
-    def __init__(self, conn_handler, conn_label, dispatcher):
+    def __init__(self, conn_handler, conn_label, dispatcher, non_blocking=False):
 
         self.conn_label = conn_label
         self.conn_handler = conn_handler
         self.dispatcher = dispatcher
+        self.non_blocking = non_blocking
         self.check_host_connection = False
         self.poller = select.poll()
         self.sigset = SIGSET()
-        self._allow_sigio(self.conn_handler)
+        self._allow_sigio(self.conn_handler, self.non_blocking)
 
         self.fd_to_socket = {
             self.conn_handler.fileno(): self.conn_handler
         }
 
     @staticmethod
-    def _allow_sigio(fd):
+    def _allow_sigio(fd, non_blocking):
         flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-        #fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_ASYNC | os.O_NONBLOCK)
-        fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_ASYNC)
+        if non_blocking:
+            fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_ASYNC | os.O_NONBLOCK)
+        else:
+            fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_ASYNC)
         fcntl.fcntl(fd, fcntl.F_SETOWN, os.getpid())
 
     def sigio_handler(self, signal_number, frame):
@@ -36,30 +39,34 @@ class ChannelAgent(object):
         if not self.check_host_connection:
             print "set mask sigio"
             self.mask_sigio()
-        self.check_host_connection = True
+            self.check_host_connection = True
 
     def mask_sigio(self):
         sigemptyset(self.sigset)
         sigaddset(self.sigset, signal.SIGIO)
-        sigprocmask(SIG_BLOCK, self.sigset, 0);
+        sigprocmask(SIG_BLOCK, self.sigset, 0)
 
     def unmask_sigio(self):
         sigemptyset(self.sigset)
         sigaddset(self.sigset, signal.SIGIO)
-        sigprocmask(SIG_UNBLOCK, self.sigset, 0);
+        sigprocmask(SIG_UNBLOCK, self.sigset, 0)
+
+    def handle_host_conn_shutdown(self):
+        self.unmask_sigio()
+        self.check_host_connection = False
 
     def handle_host_msg(self, flag):
         if flag & select.POLLIN:
-            data = self.get_request_data()
-
+            message = self.get_request_data()
+            print message
             # TODO handle IOError data
             # for example, 
-            if not data:
+            if not message:
                 print "no data recived"
                 LOG.info("Connection closed")
                 self.handle_host_conn_shutdown()
                 return
-            resp = self.dispatcher(data)
+            resp = self.dispatcher(message)
             # time.sleep(2)
             # TODO flush() function will always be blocked
             # if connection closed
@@ -69,55 +76,47 @@ class ChannelAgent(object):
             # 所以要设置超时机制，并且如果超时了，需要清空文件的缓冲区
             self.write_back_to_host(resp)
 
+        # handle device unhotpluged
+        elif flag & select.POLLHUP:
+            sys.exit(-1)
+
     def get_request_data(self):
-        try:
-            request_data = self.conn_handler.readline()
-            return request_data.strip()
-        except IOError as error:
-            print "get into read"
-            print error
-            pass
+        return self._do_read()
 
     def write_back_to_host(self, resp):
-
         try:
-            if not resp.endswith('\n'):
-                self.conn_handler.write(resp)
-                self.conn_handler.write('\n')
-                self.conn_handler.flush()
-            else:
-                self.conn_handler.write(resp)
-                self.conn_handler.flush()
-        except IOError as error:
-            print "get into write"
-            print error
-            # TODO handle IOError
+            self._do_write_back(resp)
+        except (IOError, TimeoutError):
+            return
 
-        # while True:
-        #     try:
-        #         if not resp.endswith('\n'):
-        #             self.conn_handler.write(resp)
-        #             self.conn_handler.write('\n')
-        #             self.conn_handler.flush()
-        #             break
-        #         else:
-        #             self.conn_handler.write(resp)
-        #             self.conn_handler.flush()
-        #             break
-        #     except IOError as error:
-        #         print error.errno
-        #         print errno.EAGAIN
-        #         if error.errno == errno.EAGAIN:
-        #             continue
-        #         break
-        #         #self.handle_host_conn_shutdown()
-        #         print "get into write"
-        #         print error
-        #         # TODO handle IOError
+    @timeout(2)
+    def _do_read(self):
+        while True:
+            try:
+                data = self.conn_handler.readline()
+                return data.strip()
+            except IOError as error:
+                if error.errno == errno.EAGAIN:
+                    time.sleep(0.05)
+                    continue
+                else:
+                    return None
 
-    def handle_host_conn_shutdown(self):
-        self.unmask_sigio()
-        self.check_host_connection = False
+    @timeout(2)
+    def _do_write_back(self, resp):
+        while True:
+            try:
+                if not resp.endswith('\n'):
+                    self.conn_handler.write(resp)
+                    self.conn_handler.write('\n')
+                    self.conn_handler.flush()
+                else:
+                    self.conn_handler.write(resp)
+                    self.conn_handler.flush()
+                break
+            except IOError:
+                time.sleep(0.05)
+                continue
 
     def run(self):
         signal.signal(signal.SIGIO, self.sigio_handler)
@@ -132,20 +131,13 @@ class ChannelAgent(object):
             if nfds > 0:
                 try:
                     events = self.poller.poll()
-                    print "get into poll"
                     for fd, flag in events:
                         s = self.fd_to_socket[fd]
                         if s is self.conn_handler:
                             self.handle_host_msg(flag)
 
                 except select.error as e:
-                    print e
+                    time.sleep(1)
             else:
                 print "waiting for connection..."
                 time.sleep(5)
-
-
-# if __name__ == '__main__':
-#    conn_handler = open("/dev/virtio-ports/org.qemu.guest_agent.1", "r+")
-#    ga = ChannelAgent(conn_handler, "test")
-#    ga.run()
