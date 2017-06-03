12522bcda73642b634cb1ffde455ebd5da01e3ca
fanlizhou <lizhou.fan@midea.com>
2017-06-03 18:19:49 +0800

    catch ValueError and KeyError

--- visage/dispatch/dispatcher.py
+++ visage/dispatch/dispatcher.py
@@ -17,19 +17,44 @@ class Dispatcher(object):
             backends[key] = import_object(value)
         self.backends = backends
 
+    def get_backend(self, req):
+        return request.get('backend')
+
+    def get_method(self, req):
+        return request.get('execute')
+
+    def get_args(self, req):
+        try:
+            return request.get('args')
+        except KeyError:
+            empty = ()
+            return empty
+
+    def get_kwargs(self, req):
+        try:
+            return request.get('kwargs')
+        except KeyError:
+            empty = {}
+            return empty
+
     def __call__(self, msg):
-        request = json.loads(msg)
-        backend_name = request.get('backend')
-        method_name = request.get('execute')
-        args = request.get('args')
-        kwargs = request.get('kwargs')
+        try:
+            request = json.loads(msg)
+            backend_name = self.get_backend(request)
+            method_name = self.get_method(request)
+            args = self.get_args(request)
+            kwargs = self.get_kwargs(request)
 
-        backend = self.backends[backend_name]
-        method = getattr(backend, method_name)
+            backend = self.backends[backend_name]
+            method = getattr(backend, method_name)
 
-        resp = method(*args, **kwargs)
+            resp = method(*args, **kwargs)
+            return resp
+        except ValueError as error:
+            return str(error)
+        except KeyError as error:
+            return str(error)
 
-        return resp
 
 if __name__ == '__main__':
 
